48a11f97cd6d0e813c9ce6a32645c4c5f8b77900
Abhishek <mabhishek4156@gmail.com>
2021-11-08 12:56:10 +0530

    add dash board

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 function Tables(props) {
+    const [ list, setList] = useState([])
+
+    useEffect(() => {
+        
+
+       fetch("http://localhost:3000/zafar").then((response) =>{
+           response.json().then((result)=>{
+               console.warn(result)
+               setList(result);
+           })
+       })
+    })
+    console.log(list,"smting here");
     return (
         <div>
             <h1>Tables</h1>
+            {
+                list.map((item,i)=>{
+                    return(
+                        <div>
+                            <p>{item.Name}</p>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
+   
 }
 
 export default Tables;
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+
+// import catax from '../src/catax.png'
+
+import OutlinedCard from "./card";
+
+
 
 function Transaction(props) {
-    return (
-       <div>
-            <h1>Transaction</h1>
-        </div>
-    );
+    // const [ search, setSearch] = useState("")
+
+  return (
+    <div >
+      <div  className=" items-center flex flex-col max-width-1200 ">
+        <h1 className="py-5 text- leading-6 space-y-4  text-sm font-bold text-red-900 text-4xl ">Total Transaction <span>344.14$</span></h1>
+        <h2 className="py-5 text- leading-6 space-y-4  text-sm font-bold text-blue-900 text-3xl">Today total transaction <span>4.3$</span></h2> 
+      </div>
+      <div style={{
+          
+          width:"300px",
+         
+      }}>       
+          {/* <input type="text" placeholder="Search Transections" className=" text-3xl border " /> */}
+        <h3 className="text-3xl py-5">  Total Value </h3>
+        <h3 className="text-3xl py-5"> Processing Data</h3>
+      </div>
+      <div className="border-solid space-y-4 p-1.5 text-3xl ">  
+        
+      </div>
+    </div>
+  );
 }
 
 export default Transaction;
