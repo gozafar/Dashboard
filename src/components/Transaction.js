741b3540bcc629b83731e5d23e5ae8b498fc6874
Abhishek <mabhishek4156@gmail.com>
2021-11-08 18:54:50 +0530

    add chart bar in transection

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,32 +1,50 @@
 import React from "react";
-
-// import catax from '../src/catax.png'
-
-import OutlinedCard from "./card";
-
-
+import Chart from "./Chart";
 
 function Transaction(props) {
-    // const [ search, setSearch] = useState("")
+  // const [ search, setSearch] = useState("")
 
   return (
-    <div >
-      <div  className=" items-center flex flex-col max-width-1200 ">
-        <h1 className="py-5 text- leading-6 space-y-4  text-sm font-bold text-red-900 text-4xl ">Total Transaction <span>344.14$</span></h1>
-        <h2 className="py-5 text- leading-6 space-y-4  text-sm font-bold text-blue-900 text-3xl">Today total transaction <span>4.3$</span></h2> 
+    <div>
+      <div className="  flex flex-row justify-center max-width-1200  ">
+        <div className="border-4 box-border h-32 w-250 p-4">
+          <h1 className="py-5 text- leading-6 space-y-4   font-bold text-red-900 text-4xl ">
+            Total Transaction <span className="text-green-900">344.14$</span>
+          </h1>
+        </div>
+        <div className="border-4 box-border h-32 w-250 p-4">
+          <h2 className="py-5 text- leading-6 space-y-4   font-bold text-blue-900 text-3xl">
+            Today total transaction <span className="text-green-900">4.3$</span>
+          </h2>
+        </div>
       </div>
-      <div style={{
-          
-          width:"300px",
-         
-      }}>       
-          {/* <input type="text" placeholder="Search Transections" className=" text-3xl border " /> */}
-        <h3 className="text-3xl py-5">  Total Value </h3>
+      <br />
+      <div className="  flex  justify-center   ">
+        <h3 className="text-3xl py-5"> Total Value </h3>
+      </div>
+      <div className="  flex  justify-center   ">
         <h3 className="text-3xl py-5"> Processing Data</h3>
       </div>
-      <div className="border-solid space-y-4 p-1.5 text-3xl ">  
+      <div className="flex justify-center ">
+        <h3 className="text-3xl py-5">Pending Reconciliation</h3>
+      </div>
+      <div className="flex justify-center ">
+        <h3 className="text-3xl py-5">Pending Analysis</h3>
+      </div>
+      <div className="  flex flex-row justify-center  ">
+      <Chart />
+      </div>
+      <div className="  flex flex-row justify-center " >
+      <h3 className="text-3xl py-5">Latest Transection</h3>
+      <div>
+        <table>
+          
+        </table>
+      </div>
         
       </div>
+
+      
     </div>
   );
 }
