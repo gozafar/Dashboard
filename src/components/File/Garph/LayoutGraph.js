7d12b45de4dc878106308184ecf213c1d757a4c6
zafaryab <zafaryabkham@outlook.com>
2021-11-19 17:13:36 +0530

    add chart

--- src/components/File/Garph/LayoutGraph.js
+++ src/components/File/Garph/LayoutGraph.js
@@ -1,12 +1,10 @@
 import React from "react";
-import GraphNav from "./GraphNav";
 import Heading from "../../main/Heading";
 
 function LayoutGraph({ children }) {
   return (
     <div>
       <Heading />
-      {/* <GraphNav /> */}
       {children}
     </div>
   );
