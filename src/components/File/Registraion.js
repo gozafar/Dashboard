811574fdfb3f8b0123b6bdac646f94a80ce59677
Abhishek Mishra <mabhishek4156@gmail.com>
2021-11-22 01:32:56 -0800

    add

--- src/components/File/Registraion.js
+++ src/components/File/Registraion.js
@@ -1,33 +1,34 @@
 import React from "react";
+import { DateRangePickerComponent } from "@syncfusion/ej2-react-calendars";
+import "../../style.css";
+
 
 
 function Registraion(props) {
   return (
-    <div style={{
-      background: "#f8f0f0",
-    }}>
-      <div  >
-        <div style={{
-          display:"flex",
-          flexDirection:"row"
-        }}>
-        <div>
-          <input className="search-bar" type="text" placeholder="Enter username,email,mobile number..." />
-          searchbar
+    <div  className="registration-m ">
+      
+        <div className="registration-l ">
+      
+          <div >
+          <input className="search-bar "  type="text" placeholder="Enter username,email,mobile number..." />
+          </div>
+         
+          <div className="date-p " >
+          <DateRangePickerComponent  placeholder="Enter a Date Here " />
+          </div>
+          
+ 
         </div>
-        <div>
-          filter bar
-        </div>
-        <div>
-          date symbol
-        </div>
-        </div>
-
-      </div>
-      <div>
-        
-      </div>
-     
+        <div className="reg-row">
+            <span>Register</span>
+            <span>Incomplete</span>
+            <span> Complete</span>
+            <span>Latest</span>
+            <span> My Clients</span>
+            <span>My Notes</span>
+          </div>
+    
     </div>
   );
 }
