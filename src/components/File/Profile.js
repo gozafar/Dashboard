56fd05a25951592bd15464fbf82f2e4ab4271fb1
zafaryab <zafaryabkham@outlook.com>
2021-11-24 18:50:45 +0530

    add task

--- src/components/File/Profile.js
+++ src/components/File/Profile.js
@@ -3,7 +3,7 @@ import React from "react";
 function Profile(props) {
   return (
     <div>
-      <h1>profile</h1>
+      <h1></h1>
     </div>
   );
 }
--- src/components/File/Task.js
+++ src/components/File/Task.js
@@ -1,7 +1,72 @@
 import React from "react";
-
+import {
+  IStyleSet,
+  Label,
+  ILabelStyles,
+  Pivot,
+  PivotItem,
+} from "@fluentui/react";
+const labelStyles: Partial<IStyleSet<ILabelStyles>> = {
+  root: { marginTop: 10 },
+};
 function Task(props) {
-  return <div></div>;
+  return (
+    <div>
+      <Pivot aria-label="Basic Pivot Example">
+        <PivotItem
+          headerText="My Files"
+          headerButtonProps={{
+            "data-order": 1,
+            "data-title": "My Files Title",
+          }}
+        >
+          <Label styles={labelStyles}>
+            Pivot #1
+            <div className="bg-red-50 text-black">
+              <div className="main-menu flex p-6 flex-wrap justify-evenly  ">
+                <div className="w-56 h-36 border">Registered Today</div>
+                <div className="w-56 h-36  border">Registered Yesterday</div>
+                <div className="w-56 h-36  border">Total Registred</div>
+                <div className="w-56 h-36  border ">
+                  Incomplete Registration
+                </div>
+              </div>
+            </div>
+          </Label>
+        </PivotItem>
+        <PivotItem headerText="Recent">
+          <Label styles={labelStyles}>
+            Pivot #2
+            <div className="bg-red-50 text-black">
+              <div className="main-menu flex p-6 flex-wrap justify-evenly  ">
+                <div className="w-56 h-36 border">Registered Today</div>
+                <div className="w-56 h-36  border">Registered Yesterday</div>
+                <div className="w-56 h-36  border">Total Registred</div>
+                <div className="w-56 h-36  border ">
+                  Incomplete Registration
+                </div>
+              </div>
+            </div>
+          </Label>
+        </PivotItem>
+        <PivotItem headerText="Shared with me">
+          <Label styles={labelStyles}>
+            Pivot #3
+            <div className="bg-red-50 text-black">
+              <div className="main-menu flex p-6 flex-wrap justify-evenly  ">
+                <div className="w-56 h-36 border">Registered Today</div>
+                <div className="w-56 h-36  border">Registered Yesterday</div>
+                <div className="w-56 h-36  border">Total Registred</div>
+                <div className="w-56 h-36  border ">
+                  Incomplete Registration
+                </div>
+              </div>
+            </div>
+          </Label>
+        </PivotItem>
+      </Pivot>
+    </div>
+  );
 }
 
 export default Task;
